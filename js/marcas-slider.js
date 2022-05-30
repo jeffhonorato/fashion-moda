0a7edeaef13a0111b32be719d421b8638ab0bd4b
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-05-30 10:16:52 -0300

    Ajustes

---
+++ js/marcas-slider.js
@@ -0,0 +1,27 @@
+let tempoDeRotacao = 3000;
+let boxDeSlider = 0;
+let sliders = document.querySelectorAll(".marca__box");
+let maximoDeSlider = sliders.length;
+
+
+const nextImage = () => {
+
+    sliders[boxDeSlider].classList.remove("selected")
+
+    boxDeSlider++;
+
+    if(boxDeSlider >= maximoDeSlider) {
+        boxDeSlider = 0;
+    }
+
+    sliders[boxDeSlider].classList.add("selected")
+}
+
+const marcas = () => {
+    setInterval(() => {
+        //TROCA DE IMAGEM
+        nextImage();
+    }, tempoDeRotacao)
+}
+
+window.addEventListener("load", marcas);
