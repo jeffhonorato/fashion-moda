93ba24579232d42d7f3f5636ee6b6ba8b04bacbe
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-06-07 09:14:03 -0300

    Ajustes

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -1,3 +1,20 @@
 window.sr = ScrollReveal({reset: true})
 
 sr.reveal('.cards__fem', {duration: 1000} )
+
+sr.reveal('.btn__promocao', {duration: 1000} )
+
+sr.reveal('.img__detalhe__header__fem', {duration: 1000} )
+
+sr.reveal('.adesivo__titulo__h1', {duration: 1000} )
+
+sr.reveal('.img__detalhe2__header__fem', {duration: 1000} )
+
+sr.reveal('.menu__moda__fem', {duration: 1000} )
+
+
+
+
+
+
+
