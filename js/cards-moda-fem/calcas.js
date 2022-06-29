52ff6a2b8cc793f19a73757f66fe4d1ac86bf2af
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-06-29 13:56:16 -0300

    Ajustes

--- js/cards-moda-fem/calcas.js
+++ js/cards-moda-fem/calcas.js
@@ -1,25 +1,29 @@
 // CARDS MODA FEMININA - CALÇAS // 
 const modalCard1CalcasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-calca-card1");
+    const imgPequenas = document.querySelectorAll(".js-imgPeq-card1-calca");
     imgPequenas.forEach((img) => {
         img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-calca-card1");
+            const imgGrande = document.querySelector(".js-imgGra-card1-calca");
             imgGrande.src = img.src;
         });
     });
 
-    const detalhesCard1 = document.querySelector(".js-btn1-detalhes-calca");
+    const detalhesCard1 = document.querySelector(".js-card1-calcaFem");
     detalhesCard1.addEventListener("click", (e) => {
         e.stopPropagation()
-        const modalBoxCard1 = document.querySelector(".js-modal__card1__calca");
-        modalBoxCard1.classList.add("show"); 
+        const modalBoxCard1 = document.querySelector(".js-modal-card1-calcasFem");
+        modalBoxCard1.classList.add("open"); 
+        const menuMobile = document.querySelector(".menu__mobile")
+        menuMobile.style.display = "none"; 
     });
 
-    const btnFechar = document.querySelector(".js-btn-fechar-card1-calca");
+    const btnFechar = document.querySelector(".js-btnFechar-card1-calcasFem");
     btnFechar.addEventListener("click", (e) => {
         e.stopPropagation()
-        const modalBoxCard1 = document.querySelector(".js-modal__card1__calca");
-        modalBoxCard1.classList.remove("show");
+        const modalBoxCard1 = document.querySelector(".js-modal-card1-calcasFem");
+        modalBoxCard1.classList.remove("open");
+        const menuMobile = document.querySelector(".menu__mobile")
+        menuMobile.style.display = "block"; 
         
     });
    
