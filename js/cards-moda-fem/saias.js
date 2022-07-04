40343a2588a0d866550d04a54210c3c4320096ef
Jefferson Honorato <53011109+jeffhonorato@users.noreply.github.com>
2022-07-04 10:06:39 -0300

    Ajustes

--- js/cards-moda-fem/saias.js
+++ js/cards-moda-fem/saias.js
@@ -1,397 +1,108 @@
 const modalCard1SaiasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-saia-card1");
-    imgPequenas.forEach((img) => {
+    const imgPeq = document.querySelectorAll(".js-imgPeq-card1-saia");
+    imgPeq.forEach((img) => {
         img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-saia-card1");
-            imgGrande.src = img.src;
+            const imgGra = document.querySelector(".js-imgGra-card1-saia");
+            imgGra.src = img.src;
         });
     });
 
-    const detalhesCard1 = document.querySelector(".js-btn1-detalhes-saia");
-    detalhesCard1.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox1 = document.querySelector(".js-modal__card1__saia");
-        modalBox1.classList.add("show"); 
-        
-        
+    const detalhesCard1Fem = document.querySelector(".js-card1-saiaFem");
+    detalhesCard1Fem.addEventListener("click", () => {
+        const modalCard1Fem = document.querySelector(".js-modal-card1-saiasFem");
+        modalCard1Fem.classList.add("open");
+        const menuMobile = document.querySelector(".menu__mobile")
+        menuMobile.style.display = "none"; 
     });
 
-    const btnFechar = document.querySelector(".js-btn-fechar-card1-saia");
-    btnFechar.addEventListener("click", () => {
-        const modalBox1 = document.querySelector(".js-modal__card1__saia");
-        modalBox1.classList.remove("show");
-    });
-
-    
+    const btnFecharModal1BlusaFem = document.querySelector(".js-btnFechar-card1-saiasFem");
+    btnFecharModal1BlusaFem.addEventListener("click", () => {
+        const modalCard1Fem = document.querySelector(".js-modal-card1-saiasFem");
+        modalCard1Fem.classList.remove("open");
+        const menuMobile = document.querySelector(".menu__mobile")
+        menuMobile.style.display = "block";
+    })
 }
 
 const modalCard2SaiasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-saia-card2");
-    imgPequenas.forEach((img) => {
+    const imgPeq = document.querySelectorAll(".js-imgPeq-card2-saia");
+    imgPeq.forEach((img) => {
         img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-saia-card2");
-            imgGrande.src = img.src;
+            const imgGra = document.querySelector(".js-imgGra-card2-saia");
+            imgGra.src = img.src;
         });
     });
 
-    const detalhesCard2 = document.querySelector(".js-btn2-detalhes-saia");
-    detalhesCard2.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox2 = document.querySelector(".js-modal__card2__saia");
-        modalBox2.classList.add("show"); 
-        
-        
+    const detalhesCard2Fem = document.querySelector(".js-card2-saiaFem");
+    detalhesCard2Fem.addEventListener("click", () => {
+        const modalCard2Fem = document.querySelector(".js-modal-card2-saiasFem");
+        modalCard2Fem.classList.add("open");
+        const menuMobile = document.querySelector(".menu__mobile")
+        menuMobile.style.display = "none"; 
     });
 
-    const btnFechar = document.querySelector(".js-btn-fechar-card2-saia");
-    btnFechar.addEventListener("click", () => {
-        const modalBox2 = document.querySelector(".js-modal__card2__saia");
-        modalBox2.classList.remove("show");
-    });
-
-    
+    const btnFecharModal2BlusaFem = document.querySelector(".js-btnFechar-card2-saiasFem");
+    btnFecharModal2BlusaFem.addEventListener("click", () => {
+        const modalCard2Fem = document.querySelector(".js-modal-card2-saiasFem");
+        modalCard2Fem.classList.remove("open");
+        const menuMobile = document.querySelector(".menu__mobile")
+        menuMobile.style.display = "block";
+    })
 }
 
 const modalCard3SaiasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-saia-card3");
-    imgPequenas.forEach((img) => {
+    const imgPeq = document.querySelectorAll(".js-imgPeq-card3-saia");
+    imgPeq.forEach((img) => {
         img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-saia-card3");
-            imgGrande.src = img.src;
+            const imgGra = document.querySelector(".js-imgGra-card3-saia");
+            imgGra.src = img.src;
         });
     });
 
-    const detalhesCard3 = document.querySelector(".js-btn3-detalhes-saia");
-    detalhesCard3.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox3 = document.querySelector(".js-modal__card3__saia");
-        modalBox3.classList.add("show"); 
-        
-        
+    const detalhesCard3Fem = document.querySelector(".js-card3-saiaFem");
+    detalhesCard3Fem.addEventListener("click", () => {
+        const modalCard3Fem = document.querySelector(".js-modal-card3-saiasFem");
+        modalCard3Fem.classList.add("open");
+        const menuMobile = document.querySelector(".menu__mobile")
+        menuMobile.style.display = "none"; 
     });
 
-    const btnFechar = document.querySelector(".js-btn-fechar-card3-saia");
-    btnFechar.addEventListener("click", () => {
-        const modalBox3 = document.querySelector(".js-modal__card3__saia");
-        modalBox3.classList.remove("show");
-    });
-
-    
+    const btnFecharModal3BlusaFem = document.querySelector(".js-btnFechar-card3-saiasFem");
+    btnFecharModal3BlusaFem.addEventListener("click", () => {
+        const modalCard3Fem = document.querySelector(".js-modal-card3-saiasFem");
+        modalCard3Fem.classList.remove("open");
+        const menuMobile = document.querySelector(".menu__mobile")
+        menuMobile.style.display = "block";
+    })
 }
 
 const modalCard4SaiasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-saia-card4");
-    imgPequenas.forEach((img) => {
+    const imgPeq = document.querySelectorAll(".js-imgPeq-card4-saia");
+    imgPeq.forEach((img) => {
         img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-saia-card4");
-            imgGrande.src = img.src;
+            const imgGra = document.querySelector(".js-imgGra-card4-saia");
+            imgGra.src = img.src;
         });
     });
 
-    const detalhesCard4 = document.querySelector(".js-btn4-detalhes-saia");
-    detalhesCard4.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox4 = document.querySelector(".js-modal__card4__saia");
-        modalBox4.classList.add("show"); 
-        
-        
+    const detalhesCard4Fem = document.querySelector(".js-card4-saiaFem");
+    detalhesCard4Fem.addEventListener("click", () => {
+        const modalCard4Fem = document.querySelector(".js-modal-card4-saiasFem");
+        modalCard4Fem.classList.add("open");
+        const menuMobile = document.querySelector(".menu__mobile")
+        menuMobile.style.display = "none"; 
     });
 
-    const btnFechar = document.querySelector(".js-btn-fechar-card4-saia");
-    btnFechar.addEventListener("click", () => {
-        const modalBox4 = document.querySelector(".js-modal__card4__saia");
-        modalBox4.classList.remove("show");
-    });
-
-    
+    const btnFecharModal4BlusaFem = document.querySelector(".js-btnFechar-card4-saiasFem");
+    btnFecharModal4BlusaFem.addEventListener("click", () => {
+        const modalCard4Fem = document.querySelector(".js-modal-card4-saiasFem");
+        modalCard4Fem.classList.remove("open");
+        const menuMobile = document.querySelector(".menu__mobile")
+        menuMobile.style.display = "block";
+    })
 }
 
-const modalCard5SaiasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-saia-card5");
-    imgPequenas.forEach((img) => {
-        img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-saia-card5");
-            imgGrande.src = img.src;
-        });
-    });
 
-    const detalhesCard5 = document.querySelector(".js-btn5-detalhes-saia");
-    detalhesCard5.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox5 = document.querySelector(".js-modal__card5__saia");
-        modalBox5.classList.add("show"); 
-        
-        
-    });
-
-    const btnFechar = document.querySelector(".js-btn-fechar-card5-saia");
-    btnFechar.addEventListener("click", () => {
-        const modalBox5 = document.querySelector(".js-modal__card5__saia");
-        modalBox5.classList.remove("show");
-    });
-
-    
-}
-
-const modalCard6SaiasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-saia-card6");
-    imgPequenas.forEach((img) => {
-        img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-saia-card6");
-            imgGrande.src = img.src;
-        });
-    });
-
-    const detalhesCard6 = document.querySelector(".js-btn6-detalhes-saia");
-    detalhesCard6.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox6 = document.querySelector(".js-modal__card6__saia");
-        modalBox6.classList.add("show"); 
-        
-        
-    });
-
-    const btnFechar = document.querySelector(".js-btn-fechar-card6-saia");
-    btnFechar.addEventListener("click", () => {
-        const modalBox6 = document.querySelector(".js-modal__card6__saia");
-        modalBox6.classList.remove("show");
-    });
-
-    
-}
-
-const modalCard7SaiasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-saia-card7");
-    imgPequenas.forEach((img) => {
-        img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-saia-card7");
-            imgGrande.src = img.src;
-        });
-    });
-
-    const detalhesCard7 = document.querySelector(".js-btn7-detalhes-saia");
-    detalhesCard7.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox7 = document.querySelector(".js-modal__card7__saia");
-        modalBox7.classList.add("show"); 
-        
-        
-    });
-
-    const btnFechar = document.querySelector(".js-btn-fechar-card7-saia");
-    btnFechar.addEventListener("click", () => {
-        const modalBox7 = document.querySelector(".js-modal__card7__saia");
-        modalBox7.classList.remove("show");
-    });
-
-    
-}
-
-const modalCard8SaiasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-saia-card8");
-    imgPequenas.forEach((img) => {
-        img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-saia-card8");
-            imgGrande.src = img.src;
-        });
-    });
-
-    const detalhesCard8 = document.querySelector(".js-btn8-detalhes-saia");
-    detalhesCard8.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox8 = document.querySelector(".js-modal__card8__saia");
-        modalBox8.classList.add("show"); 
-        
-        
-    });
-
-    const btnFechar = document.querySelector(".js-btn-fechar-card8-saia");
-    btnFechar.addEventListener("click", () => {
-        const modalBox8 = document.querySelector(".js-modal__card8__saia");
-        modalBox8.classList.remove("show");
-    });
-
-    
-}
-
-const modalCard9SaiasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-saia-card9");
-    imgPequenas.forEach((img) => {
-        img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-saia-card9");
-            imgGrande.src = img.src;
-        });
-    });
-
-    const detalhesCard9 = document.querySelector(".js-btn9-detalhes-saia");
-    detalhesCard9.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox9 = document.querySelector(".js-modal__card9__saia");
-        modalBox9.classList.add("show"); 
-        
-        
-    });
-
-    const btnFechar = document.querySelector(".js-btn-fechar-card9-saia");
-    btnFechar.addEventListener("click", () => {
-        const modalBox9 = document.querySelector(".js-modal__card9__saia");
-        modalBox9.classList.remove("show");
-    });
-
-    
-}
-
-const modalCard10SaiasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-saia-card10");
-    imgPequenas.forEach((img) => {
-        img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-saia-card10");
-            imgGrande.src = img.src;
-        });
-    });
-
-    const detalhesCard10 = document.querySelector(".js-btn10-detalhes-saia");
-    detalhesCard10.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox10 = document.querySelector(".js-modal__card10__saia");
-        modalBox10.classList.add("show"); 
-        
-        
-    });
-
-    const btnFechar = document.querySelector(".js-btn-fechar-card10-saia");
-    btnFechar.addEventListener("click", () => {
-        const modalBox10 = document.querySelector(".js-modal__card10__saia");
-        modalBox10.classList.remove("show");
-    });
-
-    
-}
-
-const modalCard11SaiasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-saia-card11");
-    imgPequenas.forEach((img) => {
-        img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-saia-card11");
-            imgGrande.src = img.src;
-        });
-    });
-
-    const detalhesCard11 = document.querySelector(".js-btn11-detalhes-saia");
-    detalhesCard11.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox11 = document.querySelector(".js-modal__card11__saia");
-        modalBox11.classList.add("show");  
-    });
-
-    const btnFechar = document.querySelector(".js-btn-fechar-card11-saia");
-    btnFechar.addEventListener("click", () => {
-        const modalBox11 = document.querySelector(".js-modal__card11__saia");
-        modalBox11.classList.remove("show");
-    });
-
-    
-}
-
-const modalCard12SaiasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-saia-card12");
-    imgPequenas.forEach((img) => {
-        img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-saia-card12");
-            imgGrande.src = img.src;
-        });
-    });
-
-    const detalhesCard12 = document.querySelector(".js-btn12-detalhes-saia");
-    detalhesCard12.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox12 = document.querySelector(".js-modal__card12__saia");
-        modalBox12.classList.add("show"); 
-    });
-
-    const btnFechar = document.querySelector(".js-btn-fechar-card12-saia");
-    btnFechar.addEventListener("click", () => {
-        const modalBox12 = document.querySelector(".js-modal__card12__saia");
-        modalBox12.classList.remove("show");
-    });
-
-    
-}
-
-const modalCard13SaiasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-saia-card13");
-    imgPequenas.forEach((img) => {
-        img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-saia-card13");
-            imgGrande.src = img.src;
-        });
-    });
-
-    const detalhesCard13 = document.querySelector(".js-btn13-detalhes-saia");
-    detalhesCard13.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox13 = document.querySelector(".js-modal__card13__saia");
-        modalBox13.classList.add("show"); 
-    });
-
-    const btnFechar = document.querySelector(".js-btn-fechar-card13-saia");
-    btnFechar.addEventListener("click", () => {
-        const modalBox13 = document.querySelector(".js-modal__card13__saia");
-        modalBox13.classList.remove("show");
-    });
-
-    
-}
-
-const modalCard14SaiasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-saia-card14");
-    imgPequenas.forEach((img) => {
-        img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-saia-card14");
-            imgGrande.src = img.src;
-        });
-    });
-
-    const detalhesCard14 = document.querySelector(".js-btn14-detalhes-saia");
-    detalhesCard14.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox14 = document.querySelector(".js-modal__card14__saia");
-        modalBox14.classList.add("show"); 
-    });
-
-    const btnFechar = document.querySelector(".js-btn-fechar-card14-saia");
-    btnFechar.addEventListener("click", () => {
-        const modalBox14 = document.querySelector(".js-modal__card14__saia");
-        modalBox14.classList.remove("show");
-    });
-
-    
-}
-
-const modalCard15SaiasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-saia-card15");
-    imgPequenas.forEach((img) => {
-        img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-saia-card15");
-            imgGrande.src = img.src;
-        });
-    });
-
-    const detalhesCard15 = document.querySelector(".js-btn15-detalhes-saia");
-    detalhesCard15.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox15 = document.querySelector(".js-modal__card15__saia");
-        modalBox15.classList.add("show"); 
-    });
-
-    const btnFechar = document.querySelector(".js-btn-fechar-card15-saia");
-    btnFechar.addEventListener("click", () => {
-        const modalBox15 = document.querySelector(".js-modal__card15__saia");
-        modalBox15.classList.remove("show");
-    });
-
-    
-}
 
 
 
@@ -400,14 +111,3 @@ modalCard1SaiasFem();
 modalCard2SaiasFem();
 modalCard3SaiasFem();
 modalCard4SaiasFem();
-modalCard5SaiasFem();
-modalCard6SaiasFem();
-modalCard7SaiasFem();
-modalCard8SaiasFem();
-modalCard9SaiasFem();
-modalCard10SaiasFem();
-modalCard11SaiasFem();
-modalCard12SaiasFem();
-modalCard13SaiasFem();
-modalCard14SaiasFem();
-modalCard15SaiasFem();
