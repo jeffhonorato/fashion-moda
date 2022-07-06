a989b81110b88e0bad43a9b953db8a6d71e1df87
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-07-06 09:28:15 -0300

    Ajustes

--- js/cards-moda-fem/bolsas.js
+++ js/cards-moda-fem/bolsas.js
@@ -1,261 +1,81 @@
-const modalCard1BolsasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-bolsa-card1");
-    imgPequenas.forEach((img) => {
+const modalBolsasFemCard1 = () => {
+    const imgPeq = document.querySelectorAll(".js-imgPeq-card1-bolsa");
+    imgPeq.forEach((img) => {
         img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-bolsa-card1");
-            imgGrande.src = img.src;
+            const imgGra = document.querySelector(".js-imgGra-card1-bolsa");
+            imgGra.src = img.src;
         });
     });
 
-    const detalhesCard1 = document.querySelector(".js-btn1-detalhes-bolsa");
-    detalhesCard1.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox1 = document.querySelector(".js-modal__card1__bolsa");
-        modalBox1.classList.add("show"); 
+    const detalhesCard1Fem = document.querySelector(".js-card1-bolsaFem");
+    detalhesCard1Fem.addEventListener("click", () => {
+        const modalCard1Fem = document.querySelector(".js-modal-card1-bolsasFem");
+        modalCard1Fem.classList.add("open");
+        const menuMobile = document.querySelector(".menu__mobile")
+        menuMobile.style.display = "none"; 
     });
 
-    const btnFechar = document.querySelector(".js-btn-fechar-card1-bolsa");
-    btnFechar.addEventListener("click", () => {
-        const modalBox1 = document.querySelector(".js-modal__card1__bolsa");
-        modalBox1.classList.remove("show");
-    });
-
-    
+    const btnFecharModal1BlusaFem = document.querySelector(".js-btnFechar-card1-bolsasFem");
+    btnFecharModal1BlusaFem.addEventListener("click", () => {
+        const modalCard1Fem = document.querySelector(".js-modal-card1-bolsasFem");
+        modalCard1Fem.classList.remove("open");
+        const menuMobile = document.querySelector(".menu__mobile")
+        menuMobile.style.display = "block";
+    })
 }
 
-const modalCard2BolsasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-bolsa-card2");
-    imgPequenas.forEach((img) => {
+const modalBolsasFemCard2 = () => {
+    const imgPeq = document.querySelectorAll(".js-imgPeq-card2-bolsa");
+    imgPeq.forEach((img) => {
         img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-bolsa-card2");
-            imgGrande.src = img.src;
+            const imgGra = document.querySelector(".js-imgGra-card2-bolsa");
+            imgGra.src = img.src;
         });
     });
 
-    const detalhesCard2 = document.querySelector(".js-btn2-detalhes-bolsa");
-    detalhesCard2.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox2 = document.querySelector(".js-modal__card2__bolsa");
-        modalBox2.classList.add("show"); 
+    const detalhesCard2Fem = document.querySelector(".js-card2-bolsaFem");
+    detalhesCard2Fem.addEventListener("click", () => {
+        const modalCard2Fem = document.querySelector(".js-modal-card2-bolsasFem");
+        modalCard2Fem.classList.add("open");
+        const menuMobile = document.querySelector(".menu__mobile")
+        menuMobile.style.display = "none"; 
     });
 
-    const btnFechar = document.querySelector(".js-btn-fechar-card2-bolsa");
-    btnFechar.addEventListener("click", () => {
-        const modalBox2 = document.querySelector(".js-modal__card2__bolsa");
-        modalBox2.classList.remove("show");
-    });
-
-    
+    const btnFecharModa21BlusaFem = document.querySelector(".js-btnFechar-card2-bolsasFem");
+    btnFecharModa21BlusaFem.addEventListener("click", () => {
+        const modalCard2Fem = document.querySelector(".js-modal-card2-bolsasFem");
+        modalCard2Fem.classList.remove("open");
+        const menuMobile = document.querySelector(".menu__mobile")
+        menuMobile.style.display = "block";
+    })
 }
 
-const modalCard3BolsasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-bolsa-card3");
-    imgPequenas.forEach((img) => {
+const modalBolsasFemCard3 = () => {
+    const imgPeq = document.querySelectorAll(".js-imgPeq-card3-bolsa");
+    imgPeq.forEach((img) => {
         img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-bolsa-card3");
-            imgGrande.src = img.src;
+            const imgGra = document.querySelector(".js-imgGra-card3-bolsa");
+            imgGra.src = img.src;
         });
     });
 
-    const detalhesCard3 = document.querySelector(".js-btn3-detalhes-bolsa");
-    detalhesCard3.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox3 = document.querySelector(".js-modal__card3__bolsa");
-        modalBox3.classList.add("show"); 
+    const detalhesCard3Fem = document.querySelector(".js-card3-bolsaFem");
+    detalhesCard3Fem.addEventListener("click", () => {
+        const modalCard3Fem = document.querySelector(".js-modal-card3-bolsasFem");
+        modalCard3Fem.classList.add("open");
+        const menuMobile = document.querySelector(".menu__mobile")
+        menuMobile.style.display = "none"; 
     });
 
-    const btnFechar = document.querySelector(".js-btn-fechar-card3-bolsa");
-    btnFechar.addEventListener("click", () => {
-        const modalBox3 = document.querySelector(".js-modal__card3__bolsa");
-        modalBox3.classList.remove("show");
-    });
-
-    
+    const btnFecharModa31BlusaFem = document.querySelector(".js-btnFechar-card3-bolsasFem");
+    btnFecharModa31BlusaFem.addEventListener("click", () => {
+        const modalCard3Fem = document.querySelector(".js-modal-card3-bolsasFem");
+        modalCard3Fem.classList.remove("open");
+        const menuMobile = document.querySelector(".menu__mobile")
+        menuMobile.style.display = "block";
+    })
 }
 
-const modalCard4BolsasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-bolsa-card4");
-    imgPequenas.forEach((img) => {
-        img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-bolsa-card4");
-            imgGrande.src = img.src;
-        });
-    });
-
-    const detalhesCard4 = document.querySelector(".js-btn4-detalhes-bolsa");
-    detalhesCard4.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox4 = document.querySelector(".js-modal__card4__bolsa");
-        modalBox4.classList.add("show"); 
-    });
-
-    const btnFechar = document.querySelector(".js-btn-fechar-card4-bolsa");
-    btnFechar.addEventListener("click", () => {
-        const modalBox4 = document.querySelector(".js-modal__card4__bolsa");
-        modalBox4.classList.remove("show");
-    });
-
-    
-}
-
-const modalCard5BolsasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-bolsa-card5");
-    imgPequenas.forEach((img) => {
-        img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-bolsa-card5");
-            imgGrande.src = img.src;
-        });
-    });
-
-    const detalhesCard5 = document.querySelector(".js-btn5-detalhes-bolsa");
-    detalhesCard5.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox5 = document.querySelector(".js-modal__card5__bolsa");
-        modalBox5.classList.add("show"); 
-    });
-
-    const btnFechar = document.querySelector(".js-btn-fechar-card5-bolsa");
-    btnFechar.addEventListener("click", () => {
-        const modalBox5 = document.querySelector(".js-modal__card5__bolsa");
-        modalBox5.classList.remove("show");
-    });
-
-    
-}
-
-const modalCard6BolsasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-bolsa-card6");
-    imgPequenas.forEach((img) => {
-        img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-bolsa-card6");
-            imgGrande.src = img.src;
-        });
-    });
-
-    const detalhesCard6 = document.querySelector(".js-btn6-detalhes-bolsa");
-    detalhesCard6.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox6 = document.querySelector(".js-modal__card6__bolsa");
-        modalBox6.classList.add("show"); 
-    });
-
-    const btnFechar = document.querySelector(".js-btn-fechar-card6-bolsa");
-    btnFechar.addEventListener("click", () => {
-        const modalBox6 = document.querySelector(".js-modal__card6__bolsa");
-        modalBox6.classList.remove("show");
-    });
-
-    
-}
-
-const modalCard7BolsasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-bolsa-card7");
-    imgPequenas.forEach((img) => {
-        img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-bolsa-card7");
-            imgGrande.src = img.src;
-        });
-    });
-
-    const detalhesCard7 = document.querySelector(".js-btn7-detalhes-bolsa");
-    detalhesCard7.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox7 = document.querySelector(".js-modal__card7__bolsa");
-        modalBox7.classList.add("show"); 
-    });
-
-    const btnFechar = document.querySelector(".js-btn-fechar-card7-bolsa");
-    btnFechar.addEventListener("click", () => {
-        const modalBox7 = document.querySelector(".js-modal__card7__bolsa");
-        modalBox7.classList.remove("show");
-    });
-
-    
-}
-
-const modalCard8BolsasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-bolsa-card8");
-    imgPequenas.forEach((img) => {
-        img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-bolsa-card8");
-            imgGrande.src = img.src;
-        });
-    });
-
-    const detalhesCard8 = document.querySelector(".js-btn8-detalhes-bolsa");
-    detalhesCard8.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox8 = document.querySelector(".js-modal__card8__bolsa");
-        modalBox8.classList.add("show"); 
-    });
-
-    const btnFechar = document.querySelector(".js-btn-fechar-card8-bolsa");
-    btnFechar.addEventListener("click", () => {
-        const modalBox8 = document.querySelector(".js-modal__card8__bolsa");
-        modalBox8.classList.remove("show");
-    });
-
-    
-}
-
-const modalCard9BolsasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-bolsa-card9");
-    imgPequenas.forEach((img) => {
-        img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-bolsa-card9");
-            imgGrande.src = img.src;
-        });
-    });
-
-    const detalhesCard9 = document.querySelector(".js-btn9-detalhes-bolsa");
-    detalhesCard9.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox9 = document.querySelector(".js-modal__card9__bolsa");
-        modalBox9.classList.add("show"); 
-    });
-
-    const btnFechar = document.querySelector(".js-btn-fechar-card9-bolsa");
-    btnFechar.addEventListener("click", () => {
-        const modalBox9 = document.querySelector(".js-modal__card9__bolsa");
-        modalBox9.classList.remove("show");
-    });
-
-    
-}
-
-const modalCard10BolsasFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-modal-imgP-bolsa-card10");
-    imgPequenas.forEach((img) => {
-        img.addEventListener("click", () => {
-            const imgGrande = document.querySelector(".js-modal-imgG-bolsa-card10");
-            imgGrande.src = img.src;
-        });
-    });
-
-    const detalhesCard10 = document.querySelector(".js-btn10-detalhes-bolsa");
-    detalhesCard10.addEventListener("click", (e) => {
-        e.stopPropagation()
-        const modalBox10 = document.querySelector(".js-modal__card10__bolsa");
-        modalBox10.classList.add("show"); 
-    });
-
-    const btnFechar = document.querySelector(".js-btn-fechar-card10-bolsa");
-    btnFechar.addEventListener("click", () => {
-        const modalBox10 = document.querySelector(".js-modal__card10__bolsa");
-        modalBox10.classList.remove("show");
-    });
-
-    
-}
-
-
-modalCard1BolsasFem();
-modalCard2BolsasFem();
-modalCard3BolsasFem();
-modalCard4BolsasFem();
-modalCard5BolsasFem();
-modalCard6BolsasFem();
-modalCard7BolsasFem();
-modalCard8BolsasFem();
-modalCard9BolsasFem();
-modalCard10BolsasFem();
+modalBolsasFemCard1()
+modalBolsasFemCard2()
+modalBolsasFemCard3()
