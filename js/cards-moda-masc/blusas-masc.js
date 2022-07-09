7117a5b83c63c6d71dc5ebc481c9e0ae54f10ece
Jefferson Honorato <53011109+jeffhonorato@users.noreply.github.com>
2022-07-09 10:38:41 -0300

    Ajustes

--- js/cards-moda-masc/blusas-masc.js
+++ js/cards-moda-masc/blusas-masc.js
@@ -57,36 +57,67 @@ modalBlusasMascCard1 = () => {
     });
 } 
 
+modalBlusasMascCard1 = () => {
+    const imgPequenas = document.querySelectorAll(".js-imgPeq-blusasMasc-card2");
+    imgPequenas.forEach((img) => {
+        img.addEventListener("click", () => {
+            const imgGrande = document.querySelector(".js-imgGra-blusasMasc-card2");
+            imgGrande.src = img.src;
+        });
+    });
 
-// modalBlusasMascCard2 = () => {
-//     const imgPequenas = document.querySelectorAll(".js-imgPeq-blusasMasc-card2");
-//     imgPequenas.forEach((img) => {
-//         img.addEventListener("click", () => {
-//             const imgGrande = document.querySelector(".js-imgGra-blusasMasc-card2");
-//             imgGrande.src = img.src;
-//         })2
-//     });
+    const detalhesCard2 = document.querySelector(".js-blusasMasc-card2");
+    detalhesCard2.addEventListener("click", (e) => {
+        e.stopPropagation()
+        const modalBox2 = document.querySelector(".js-modal-blusasMasc-card2");
+        modalBox2.classList.add("open");
+        const menuMobile = document.querySelector(".menu__mobile")
+        menuMobile.style.display = "none";
+    });
 
-//     const detalhesCard2 = document.querySelector(".js-blusasMasc-card2");
-//     detalhesCard2.addEventListener("click", (e) => {
-//         e.stopPropagation()
-//         const modalBox2 = document.querySelector(".js-modal-blusasMasc-card2");
-//         modalBox2.classList.add("open");
-//         // const menuMobile = document.querySelector(".menu__mobile")
-//         // menuMobile.style.display = "none"; 
-//     });
+    const btnFechar = document.querySelector(".js-btnFechar--blusasMasc-card2");
+    btnFechar.addEventListener("click", (e) => {
+        e.stopPropagation()
+        const modalBox2 = document.querySelector(".js-modal-blusasMasc-card2");
+        modalBox2.classList.remove("open");
+        const menuMobile = document.querySelector(".menu__mobile")
+        menuMobile.style.display = "block";
+    });
+} 
 
-//     const btnFechar = document.querySelector(".js-btnFechar--blusasMasc-card2");
-//     btnFechar.addEventListener("click", (e) => {
-//         e.stopPropagation()
-//         const modalBox2 = document.querySelector(".js-modal-blusasMasc-card2");
-//         modalBox2.classList.remove("open");
-//         // const menuMobile = document.querySelector(".menu__mobile")
-//         // menuMobile.style.display = "block";
-//     });
-// }  
+modalBlusasMascCard3 = () => {
+    const imgPequenas = document.querySelectorAll(".js-imgPeq-blusasMasc-card3");
+    imgPequenas.forEach((img) => {
+        img.addEventListener("click", () => {
+            const imgGrande = document.querySelector(".js-imgGra-blusasMasc-card3");
+            imgGrande.src = img.src;
+        });
+    });
+
+    const detalhesCard3 = document.querySelector(".js-blusasMasc-card3");
+    detalhesCard3.addEventListener("click", (e) => {
+        e.stopPropagation()
+        const modalBox3 = document.querySelector(".js-modal-blusasMasc-card3");
+        modalBox3.classList.add("open");
+        const menuMobile = document.querySelector(".menu__mobile")
+        menuMobile.style.display = "none";
+    });
+
+    const btnFechar = document.querySelector(".js-btnFechar--blusasMasc-card3");
+    btnFechar.addEventListener("click", (e) => {
+        e.stopPropagation()
+        const modalBox3 = document.querySelector(".js-modal-blusasMasc-card3");
+        modalBox3.classList.remove("open");
+        const menuMobile = document.querySelector(".menu__mobile")
+        menuMobile.style.display = "block";
+    });
+} 
+
+
+ 
 
 
 
 modalBlusasMascCard1();
 modalBlusasMascCard2();
+modalBlusasMascCard3();
